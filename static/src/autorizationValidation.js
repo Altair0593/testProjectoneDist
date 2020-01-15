b0638bbe4469e89d4aeb43dc1031eb37ec01879a
Denis Tkachenko <denistkachenko.official@gmail.com>
2020-01-15 12:32:20 +0300

    Change rules of validation

--- static/src/autorizationValidation.js
+++ static/src/autorizationValidation.js
@@ -1,49 +1,68 @@
-var registrationBtn = document.getElementById("registrationBtn");
+let registrationBtn = document.getElementById("registrationBtn");
 registrationBtn.addEventListener("click", getValidation);
-
-function getElementValue() {
-    var login = document.getElementById("login").value;
-    var password1 = document.getElementById("password1").value;
-    var password2 = document.getElementById("password2").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    return { login: login, password: password1, password2: password2, email: email, phone: phone }
+let err = "";
+let User = function () {
+    this.login = document.getElementById("login").value.replace(/\s/g, '');
+    this.password = document.getElementById("password1").value;
+    this.password2 = document.getElementById("password2").value;
+    this.email = document.getElementById("email").value;
+    this.phone = document.getElementById("phone").value.replace(/[+()-\s]/g, '');
 }
+
 function getValidation() {
-    var elementValue = getElementValue();
-    if (checklogin(elementValue.login) && checkPass(elementValue.password, elementValue.password2) && checkPhone(elementValue.phone, "+380")) {
+    let elementValue = new User();
+    let message = document.getElementById("message");
+    console.log(elementValue);
+    if (checkEmail(elementValue.email) && checklogin(elementValue.login) && checkPass(elementValue.password, elementValue.password2) && checkPhone(elementValue.phone, "380")) {
         delete elementValue.password2;
         console.log("Регистрация успешна");
-        var xhr = new XMLHttpRequest();
+        console.log(elementValue);
+        let xhr = new XMLHttpRequest();
         xhr.open("POST", "/registration");
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.send(JSON.stringify(elementValue));
         xhr.onreadystatechange = function () {
-            if (xhr.status == 400) {
-                alert("user already exist");
+            if (xhr.status === 400) {
+                message.innerHTML = "Такой логин уже существует!";
+            } else {
+                message.innerHTML = "Регистрация успешна";
             }
         }
-
-        }
+    }
+    message.innerHTML = err;
 }
+
 function checklogin(login) {
     if (login.length < 1 || !isNaN(Number(login[0]))) {
-        console.log("Неверный логин");
+        err = "Некорректный логин";
         return false;
     }
     return true;
 }
+
 function checkPass(pass1, pass2) {
-    if (pass1.length <= 5 || pass1 !== pass2) {
-        console.log("Неверный пароль");
+    if (pass1.length < 5 || pass1 !== pass2) {
+        err = "Некорректный пароль";
         return false;
     }
     return true;
 }
+
 function checkPhone(phone, countriCode) {
-    if (phone.length !== 13) {
-        console.log("Неверный телефон");
+    if (phone.length !== 12 || phone.substring(0, 3) != countriCode) {
+        err = "Некорректныйтелефон";
         return false;
     }
     return true;
+}
+function checkEmail(email) {
+    if (email.includes("@")) {
+        let domen=email.substring(email.indexOf("@"), email.length);
+        if(domen.length>2){
+            return true;
+        }
+        
+    }
+    err="Некорректный e-mail"
+    return false;
 }
